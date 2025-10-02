aa7ac7e956562612afbcaa97cd784b3761d1a4f8
Phuc Le <lhphuc.2410@gmail.com>
2025-10-02 14:21:12 +0700

    validate view recruitment

--- hrm_recruitment/__manifest__.py
+++ hrm_recruitment/__manifest__.py
@@ -10,6 +10,9 @@
         "security/ir.model.access.csv",
         "views/hrm_applicant_views.xml",
         "views/hrm_job_views.xml",
-        "views/hrm_menu_views.xml",    ],
+        "views/hrm_menu_views.xml", 
+        "views/hrm_resume_line_views.xml"
+        ],
+    
     'application': True,
 }
