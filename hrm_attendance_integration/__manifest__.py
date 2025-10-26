73d17288b55307d0f22e0e4ac7250a3591db35b2
Phuc Le <lhphuc.2410@gmail.com>
2025-10-26 14:47:15 +0700

    add cron job auto check out

--- hrm_attendance_integration/__manifest__.py
+++ hrm_attendance_integration/__manifest__.py
@@ -10,7 +10,8 @@
         "security/ir.model.access.csv",
         # "views/hrm_attendance_views.xml",
         "views/hrm_attendance_menu_vỉews.xml",
-        "security/hr_attendance_security.xml.xml"
+        "security/hr_attendance_security.xml.xml",
+        "data/cron.xml",
         ],
     'application': True,
 }
