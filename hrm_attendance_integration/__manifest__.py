3cd9377ebf811a63198504ec6b40fe040d57e1f5
tbdung <dungtranba0512@gmail.com>
2025-11-14 21:58:48 +0700

    add menu

--- hrm_attendance_integration/__manifest__.py
+++ hrm_attendance_integration/__manifest__.py
@@ -9,7 +9,7 @@
     "data": [
         "security/ir.model.access.csv",
         # "views/hrm_attendance_views.xml",
-        "views/hrm_attendance_menu_vỉews.xml",
+        "views/hrm_attendance_menu_views.xml",
         "security/hr_attendance_security.xml.xml",
         "data/cron.xml",
         ],
